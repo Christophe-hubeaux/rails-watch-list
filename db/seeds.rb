b8ccc2363b9adce088ec34f34eef756787be46bf
christophe hubeaux <ch.hubeaux@gmail.com>
2024-03-08 16:13:45 +0100

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "open-uri"
+
 Bookmark.destroy_all
 Movie.destroy_all
 List.destroy_all
 
-10.times { Movie.create(title: Faker::Movie.title, overview: Faker::Movie.quote, poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: rand(1...10)) }
-# List.create(name: "My favourite movies")
-# List.create(name: "Movies yet to watch")
-# List.create(name: "Movies I dislike")
+10.times { Movie.create(title: Faker::Movie.unique.title, overview: Faker::Movie.unique.quote, poster_url: "", rating: rand(1...10)) }
 
+file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+file2 = URI.open("https://st3.depositphotos.com/11433382/14401/i/600/depositphotos_144011201-stock-photo-popcorn-and-drinks-in-glasses.jpg")
+file3 = URI.open("https://st2.depositphotos.com/1046535/7921/i/950/depositphotos_79216310-stock-photo-girl-eating-popcorn-in-cinema.jpg")
+
+firstList = List.create(name: "Movies I dislike")
+firstList.photo.attach(io: file, filename: "file1.png", content_type: "image/png")
+firstList.save
+secondList = List.create(name: "Movies I like")
+secondList.photo.attach(io: file2, filename: "file2.png", content_type: "image/png")
+secondList.save
+thirdList = List.create(name: "Movies 2024")
+thirdList.photo.attach(io: file3, filename: "file3.png", content_type: "image/png")
+thirdList.save
